ac469a05888c71fa181548a9a346e3b95c47f01a
Coby Tamayo <ctamayo@sitecrafting.com>
2018-08-20 14:44:04 -0700

    check for autoload file in well-known places

--- yaml-fixtures.php
+++ yaml-fixtures.php
@@ -13,6 +13,15 @@ if ( ! defined('WP_CLI') || ! WP_CLI ) {
 
 use YamlFixtures\FixtureCommand;
 
-require_once __DIR__ . '/vendor/autoload.php';
+// check for autoload file in well-known places
+foreach([
+  __DIR__ . '/vendor/autoload.php',
+  __DIR__ . '/../../autoload.php',
+] as $autoload) {
+  if (file_exists($autoload)) {
+    require_once $autoload;
+    break;
+  }
+}
 
 WP_CLI::add_command('fixture', new FixtureCommand());
