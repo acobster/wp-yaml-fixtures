78befc3d61b7a14a860d2f023945d45955df04d1
Coby Tamayo <ctamayo@sitecrafting.com>
2019-02-15 10:36:46 -0800

    install SQL parser, require autoloader even outside WP-CLI

--- yaml-fixtures.php
+++ yaml-fixtures.php
@@ -6,22 +6,33 @@
  * for scaffolding and testing your WordPress tools
  */
 
-// only take effect on the command line
-if ( ! defined('WP_CLI') || ! WP_CLI ) {
-  return;
-}
-
 use YamlFixtures\FixtureCommand;
 
-// check for autoload file in well-known places
-foreach ([
+$autoloadPaths = [
   __DIR__ . '/vendor/autoload.php',
   __DIR__ . '/../../autoload.php',
-] as $autoload) {
+];
+
+if (
+  defined('ABSPATH')
+  && !in_array(ABSPATH . '/vendor/autoload.php', $autoloadPaths)
+) {
+  $autoloadPaths[] = ABSPATH . '/vendor/autoload.php';
+}
+
+// check for autoload file in well-known places
+foreach ($autoloadPaths as $autoload) {
   if (file_exists($autoload)) {
     require_once $autoload;
     break;
   }
 }
+
+
+// WP_CLI declarations should only take effect on the command line
+if ( ! defined('WP_CLI') || ! WP_CLI ) {
+  return;
+}
+
 
 WP_CLI::add_command('fixture', new FixtureCommand());
